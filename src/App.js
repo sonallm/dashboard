c4c898e2abfddcc8fb5049d589c4fb71a8cf3ddb
Sonal Muthal <sonalmuthal@Sonals-MacBook-Pro.local>
2023-06-19 21:19:26 -0700

    added some components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Game from './components/Game';
+
+function Button1() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count+1);
+  }
+  return (
+    <button onClick={handleClick}>Clicked {count} times</button>
+  );
+}
+
+function Button2({bothCount, handleEitherClick}) {
+  return (
+    <button onClick={handleEitherClick}>Clicked {bothCount} times</button>
+  );
+}
 
 function App() {
+  const [bothCount, setBothCount] = useState(0);
+  function handleEitherClick () {
+    setBothCount(bothCount + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Section">
+        <h5>Counters that update separately</h5>
+        <Button1/>
+        <Button1/>
+      </div>
+      <div className="space"></div>
+      <div className="Section">
+        <h5>Counters that update together</h5>
+        <Button2 bothCount={bothCount} handleEitherClick={handleEitherClick}/>
+        <Button2 bothCount={bothCount} handleEitherClick={handleEitherClick}/>
+      </div>
+      <div className="space"></div>
+      <div className="Section">
+        <h5>Tic Tac Toe</h5>
+        <Game/>
+      </div>
     </div>
   );
 }
